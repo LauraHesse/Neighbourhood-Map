792447863432510d4986e59984d552904c235ea3
LauraHesse <laura.hesse@t-systems.com>
2017-07-10 00:01:05 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var ViewModel = function(){
   //self.query are observables - which are functions - you need to call them without any arguments to get their values.
   self.query = ko.observable('');
 
+
   self.searchResults = ko.computed(function() {
       var q = self.query();
       return locationInfo.filter(function(i) {
@@ -16,7 +17,9 @@ var ViewModel = function(){
   //It basically triggers all of the click events on the marker
   self.listClicker = function(locationInfo){
     google.maps.event.trigger(locationInfo.marker, 'click');
+    $('.side-nav').animate({width:'toggle'},350);
   };
+
 };
 
 ko.applyBindings(new ViewModel());
